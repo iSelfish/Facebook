c8e21e3fb036807cf17e17de0d71f2e2697b1ce7
dmytro_kolos <dmytro_kolos@epam.com>
2020-02-07 15:55:16 +0200

    added serenity

---
+++ src/test/java/MemberStatusLiveTest.java
@@ -0,0 +1,16 @@
+
+import net.serenitybdd.junit.runners.SerenityRunner;
+import net.thucydides.core.annotations.Steps;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+@RunWith(SerenityRunner.class)
+public class MemberStatusLiveTest {
+    @Steps
+    private int k = 120;
+
+    @Test
+    public void membersShouldStartWithBronzeStatus() {
+        System.out.println("teAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAASSAASsst");
+    }
+}
--- src/test/java/facebook/FacebookTest.java
+++ src/test/java/facebook/FacebookTest.java
@@ -1,12 +1,15 @@
 package facebook;
 
 import core.DriverManage;
+import net.serenitybdd.junit.runners.SerenityRunner;
+import org.junit.runner.RunWith;
 import pages.StartPage;
 import org.apache.log4j.BasicConfigurator;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
+@RunWith(SerenityRunner.class)
 public class FacebookTest {
     @Before
     public void basicConfigure() {
